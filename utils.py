9f1ed65279dfafa5e4f41ebd4fb5556a468fbae8
Hayden <hjhawley7@gmail.com>
2025-06-13 10:06:22 -0600

    80s

--- utils.py
+++ utils.py
@@ -6,4 +6,6 @@ def clean_track(title: str) -> str:
 
 def clean_artist(artist_name: str) -> str:
     # Cleans up artist's name for improved matching.
+    if artist_name == ("The The"): # annoying edge case
+        return artist_name
     return artist_name.replace('The ', '').replace('&', 'and')
